6d31472bc775e74fa4df4c754996565da83cc478
Marcos Vinicius <marcos.v.silva@live.com>
2019-05-13 21:58:25 -0300

    Implements filters

--- project.py
+++ project.py
@@ -1,27 +1,29 @@
 import cv2
 import numpy as np
-import scipy         
 
 exam = cv2.VideoCapture('Exams/pupilometer.mp4')
 
 while (1):
     ret, frame  = exam.read()
+    b, g, r = cv2.split(frame)
+
+    kSize = 5
+    frame_blur = cv2.blur(g, (kSize, kSize))
+    frame_laplacian = cv2.Laplacian(frame_blur, cv2.CV_8UC1)
+    test, frame_black = cv2.threshold(frame_laplacian, 0, 255, cv2.THRESH_BINARY)
+    frame_black = frame_laplacian - frame_black
+    #cv2.imshow('test', frame_black)
+
+    pupil = cv2.HoughCircles(frame_black, cv2.HOUGH_GRADIENT, 40, 10, param1=50, param2=60, minRadius=30, maxRadius=70)
+    pupil = np.uint16(np.around(pupil))
+
+    for i in pupil[0,:]:
+        cv2.circle(frame_black,(i[0],i[1]),i[2],(0,255,0),2)
+        cv2.circle(frame_black,(i[0],i[1]),2,(0,0,255),3)
+
     cv2.namedWindow('video', cv2.WINDOW_NORMAL)
+    cv2.imshow('video', frame_black)
 
-    frame = cv2.medianBlur(frame, 5)
-
-    #frame = cv2.cvtColor(frame[2], cv2.COLOR_GRAY2BGR)
-    
-    #circles = cv2.HoughCircles(frame,cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=0,maxRadius=0)
-
-
-    # pupil = np.uint16(np.around(pupil))
-
-    # for i in pupil:
-    #     cv2.circle(frame, (i[0], i[1]), i[2], (0,255,0), 2)
-    #     cv2.circle(frame, (i[0], i[1]), 2, (0,0,255), 3)*/*
-
-    cv2.imshow('video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
