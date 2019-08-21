c1b45382340da7db0049fedb53f05eb8fae558a5
Marcos Vinicius <marcos.v.silva@live.com>
2019-08-21 00:43:08 -0300

    New generation algorithm

--- pupilometer/main.py
+++ pupilometer/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import os
+import time
 
 class Main:
     def __init__(self):
@@ -8,20 +9,18 @@ class Main:
         self.exams = os.listdir(self.dataset_path)
 
         # Parameter Definition
-        self.max_height = 10
-        self.max_width = 10
-
-        self.whitening = 0.4
+        self.whitening = 0.2
 
         self.canny_threshold1 = 10
         self.canny_threshold2 = 50
 
-        self.hough_dp = 40
+        self.hough_dp = 30
         self.hough_minDist = 40
-        self.hough_param1 = 50
-        self.hough_param2 = 60
+        self.hough_param1 = 10
+        self.hough_param2 = 20
+
         self.hough_minRadius = 40
-        self.hough_maxRadius = 300
+        self.hough_maxRadius = 50
 
     def start_process(self):
         for exam in self.exams:
@@ -29,18 +28,18 @@ class Main:
             self.pupillary_analysis(video)
 
     def pupillary_analysis(self, exam):
-        while exam.isOpened():
+        pause = False
+        while pause and exam.isOpened():
             ret, frame = exam.read()
 
             frame = np.array(frame)
-            cv2.resize(frame, (self.max_height, self.max_width)).flatten()
 
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             final = np.copy(gray)
 
-            gray = np.array(255 * (gray / 255) ** self.whitening, dtype='uint8')
+            whitening = np.array(255 * (final / 255) ** self.whitening, dtype='uint8')
 
-            edge = cv2.Canny(gray, threshold1=self.canny_threshold1, threshold2=self.canny_threshold2)
+            edge = cv2.Canny(whitening, threshold1=self.canny_threshold1, threshold2=self.canny_threshold2)
 
             pupil = cv2.HoughCircles(edge, cv2.HOUGH_GRADIENT, dp=self.hough_dp, minDist=self.hough_minDist,
                                      param1=self.hough_param1, param2=self.hough_param2,
@@ -50,19 +49,25 @@ class Main:
                 pupil = np.uint16(np.around(pupil))
 
                 for i in pupil[0, :]:
-                    cv2.circle(gray, (i[0], i[1]), i[2], (0, 255, 0), 2)
-                    cv2.circle(gray, (i[0], i[1]), 2, (0, 0, 255), 3)
+                    cv2.circle(final, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                    cv2.circle(final, (i[0], i[1]), 2, (0, 0, 255), 3)
 
-            img_final1 = cv2.hconcat([final, edge])
-            cv2.resize(final, img_final1.shape).flatten()
-            img_final = cv2.hconcat([img_final1, gray])
+            img_final1 = cv2.hconcat([self.resize(gray), self.resize(whitening)])
+            img_final2 = cv2.hconcat([self.resize(edge), self.resize(final)])
+            img_final = cv2.vconcat([img_final1, img_final2])
 
             cv2.namedWindow('Progress', cv2.WINDOW_NORMAL)
             cv2.imshow('Progress', img_final)
-            cv2.waitKey(1)
+
+            if cv2.waitKey(1) & 0xFF == ord('p'):  # Pause
+                pause = not pause
 
         exam.release()
         cv2.destroyAllWindows
+
+    @staticmethod
+    def resize(figure):
+        return cv2.resize(figure, (0, 0), fx=0.5, fy=0.5)
 
 
 if __name__ == '__main__':
