26c724f92ec2067aa5fde1b0da84e95cb12538e8
Marcos Vinicius <marcos.v.silva@live.com>
2019-09-10 17:13:41 -0300

    Implementing New Filter

--- pupilometer/main.py
+++ pupilometer/main.py
@@ -3,7 +3,8 @@ import cv2
 import os
 import time
 
-semicircle_positions = ('northeast', 'southwest', 'east', 'south', 'southeast')
+# semicircle_positions = ('northeast', 'southwest', 'east', 'south', 'southeast')
+semicircle_positions = ('east', 'west', 'south')
 
 
 class Main:
@@ -27,17 +28,17 @@ class Main:
         # Selection best ellipse parameters
         self.best_area_height = (50, 300)
         self.best_area_width = (50, 300)
-        self.radius_size = (40, 65)
-        self.edge_threshold = 180
-        self.radius_validate_threshold = 5
+        self.radius_size = (30, 80)
+        self.edge_threshold = 200
+        self.radius_validate_threshold = 3
 
         # Circle draw parameters
         self.color_circle = (255, 255, 0)
         self.thickness_circle = 3
 
         # Others parameters
-        self.save_output = True
-        self.save_threshold_output = True
+        self.save_output = False
+        self.save_threshold_output = False
         self.sleep_pause = 3
 
     def start_process(self):
@@ -59,20 +60,32 @@ class Main:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             gaussian = cv2.GaussianBlur(gray, self.size_filter_gaussian, self.type_gaussian)
             median = cv2.medianBlur(gaussian, self.size_median)
-            threshold = cv2.threshold(median, self.thresh_threshold, self.maxvalue_threshold, cv2.THRESH_BINARY_INV)[1]
+
+            kernel = np.ones((5, 5), np.uint8)
+            erode_one = cv2.erode(median, kernel=kernel, iterations=1)
+            dilate_one = cv2.dilate(erode_one, kernel=kernel, iterations=1)
+            threshold_one = cv2.threshold(dilate_one, self.thresh_threshold,
+                                          self.maxvalue_threshold, cv2.THRESH_BINARY_INV)[1]
+
+            erode_two = cv2.erode(threshold_one, kernel=kernel, iterations=1)
+            dilate_two = cv2.dilate(erode_two, kernel=kernel, iterations=1)
+            threshold_two = cv2.threshold(dilate_two, self.thresh_threshold,
+                                          self.maxvalue_threshold, cv2.THRESH_BINARY_INV)[1]
+
             final = np.copy(gray)
 
-            contours = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+            contours = cv2.findContours(threshold_two, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
             contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
 
-            center, radius = self.best_center(image=threshold, contours=contours)
+            img_center, center, radius = self.best_center(image=threshold_two, contours=contours)
 
             if center is not None and radius > 0:
                 cv2.circle(final, center, radius, self.color_circle, self.thickness_circle)
 
             img_final1 = cv2.hconcat([self.resize(gray), self.resize(gaussian), self.resize(median)])
-            img_final2 = cv2.hconcat([self.resize(gray), self.resize(threshold), self.resize(final)])
-            img_final = cv2.vconcat([img_final1, img_final2])
+            img_final2 = cv2.hconcat([self.resize(erode_one), self.resize(dilate_one), self.resize(threshold_one)])
+            img_final3 = cv2.hconcat([self.resize(erode_two), self.resize(dilate_two), self.resize(final)])
+            img_final = cv2.vconcat([img_final1, img_final2, img_final3])
 
             cv2.namedWindow('Training', cv2.WINDOW_NORMAL)
             cv2.imshow('Training', img_final)
@@ -83,7 +96,7 @@ class Main:
 
             if self.save_threshold_output:
                 threshold_output = "%s/%s_%03d.png" % (self.threshold_path, self.threshold_output, number_frame)
-                cv2.imwrite(threshold_output, threshold)
+                cv2.imwrite(threshold_output, threshold_two)
 
             if cv2.waitKey(1) & 0xFF == ord('p'):  # Pause
                 time.sleep(self.sleep_pause)
@@ -94,19 +107,23 @@ class Main:
     def best_center(self, image, contours):
         center = None
         rad = 0
+        new_image = np.copy(image)
         for contour in contours:
             (x, y, w, h) = cv2.boundingRect(contour)
             center = (x + int(w / 2), y + int(h / 2))
             lin, col = image.shape
             if center[0] < lin and center[1] < col:
+                cv2.circle(new_image, center, 9, (0, 0, 0), self.thickness_circle)
+
                 radius = []
                 for direction in semicircle_positions:
-                    radius.append(self.calculate_radius(image=image, center=center, direction=direction))
+                    new_image, radio = self.calculate_radius(image=new_image, center=center, direction=direction)
+                    radius.append(radio)
 
                 if self.validate_radius(radius):
                     rad = int(np.array(radius).max())
                     break
-        return center, rad
+        return new_image, center, rad
 
     def validate_radius(self, radius):
         validate = 0
@@ -116,6 +133,7 @@ class Main:
         return validate >= self.radius_validate_threshold
 
     def calculate_radius(self, image, center, direction):
+        new_image = np.copy(image)
         lin, col = image.shape
         x, y = center
         radius = calc_radius = 0
@@ -123,25 +141,22 @@ class Main:
 
         while (1 < x < lin-1) and (1 < y < col-1):
             if direction == 'east':
-                y += 1
-            elif direction == 'south':
-                x -= 1
-            elif direction == 'northeast':
                 x += 1
-                y += 1
-            elif direction == 'southeast':
+            elif direction == 'west':
                 x -= 1
-                y += 1
-            elif direction == 'southwest':
-                x -= 1
+            elif direction == 'north':
                 y -= 1
+            elif direction == 'south':
+                y += 1
+
+            cv2.circle(new_image, (x, y), 3, (0, 0, 0), self.thickness_circle)
 
             calc_radius += 1
             if abs(int(image[x][y]) - init) > self.edge_threshold:
                 radius = calc_radius
                 break
 
-        return radius
+        return new_image, radius
 
     @staticmethod
     def resize(figure):
