c75e89df08885e5e3e3dd80ca45c1e85d21ff963
Marcos Vinicius <marcos.v.silva@live.com>
2019-09-17 14:56:36 -0300

    alter noise filters

--- pupilometer/noise.py
+++ pupilometer/noise.py
@@ -13,16 +13,24 @@ class Noise:
         line, col = frame.shape
         new_frame = np.copy(frame)
 
+        cv2.drawContours(new_frame, contours, -1, (0, 0, 0), 10)
+
         for contour in contours:
             (x, y, w, h) = cv2.boundingRect(contour)
-            if w * h < self.min_area:
-                while x <= w and x < line:
-                    while y <= h and y < col:
-                        if new_frame[y, x] == 255:
-                            new_frame = 0
-                        else:
-                            new_frame = 255
-                        y += 1
-                    x += 1
+
+
+            # if w * h < self.min_area:
+            #     for i in range(w):
+            #         for j in range(h):
+            #             position = x+i
+            #             position2 = y+j
+            #
+            #             if position >= line or position2 >= col:
+            #                 break
+            #
+            #             if new_frame[position, position2] == 255:
+            #                 new_frame[position, position2] = 0
+            #             else:
+            #                 new_frame[position, position2] = 255
 
         return new_frame
