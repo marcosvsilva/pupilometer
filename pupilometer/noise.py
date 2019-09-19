0bccf33571133dd8678c9d8212d51fc22af4777f
Marcos Vinicius <marcos.v.silva@live.com>
2019-09-18 22:25:29 -0300

    Reform noise canceller

--- pupilometer/noise.py
+++ pupilometer/noise.py
@@ -13,24 +13,17 @@ class Noise:
         line, col = frame.shape
         new_frame = np.copy(frame)
 
-        cv2.drawContours(new_frame, contours, -1, (0, 0, 0), 10)
-
         for contour in contours:
             (x, y, w, h) = cv2.boundingRect(contour)
+            if w * h < self.min_area:
+                for i in range(w):
+                    for j in range(h):
+                        position = x+i
+                        position2 = y+j
 
+                        if position >= line or position2 >= col:
+                            break
 
-            # if w * h < self.min_area:
-            #     for i in range(w):
-            #         for j in range(h):
-            #             position = x+i
-            #             position2 = y+j
-            #
-            #             if position >= line or position2 >= col:
-            #                 break
-            #
-            #             if new_frame[position, position2] == 255:
-            #                 new_frame[position, position2] = 0
-            #             else:
-            #                 new_frame[position, position2] = 255
+                        new_frame[position, position2] = 0
 
         return new_frame
