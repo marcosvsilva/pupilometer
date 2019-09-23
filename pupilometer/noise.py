606d841ea96a73a2d3b747364a0a40683e53543d
Marcos Vinicius <marcos.v.silva@live.com>
2019-09-23 15:35:05 -0300

    implementing noise handling where found a false center ellipse

--- pupilometer/noise.py
+++ pupilometer/noise.py
@@ -1,29 +1,41 @@
 import numpy as np
 import cv2
+from ellipse import Ellipse
 
 
 class Noise:
     def __init__(self, min_area):
         self.min_area = min_area
+        self.pixel_remove = 255
+        self.new_pixel = 0
+        self.ellipse = Ellipse()
 
-    def remove_noise(self, frame, contours):
+    def treatment_noise(self, frame, contours):
         if frame is None:
             raise Exception("Frame is none!")
 
-        line, col = frame.shape
         new_frame = np.copy(frame)
 
-        for contour in contours:
-            (x, y, w, h) = cv2.boundingRect(contour)
-            if w * h < self.min_area:
-                for i in range(w):
-                    for j in range(h):
-                        position = x+i
-                        position2 = y+j
+        center, _ = self.ellipse.search_ellipse(frame, contours)
 
-                        if position >= line or position2 >= col:
-                            break
+        if frame[center[0], center[1]] == self.pixel_remove:
+            new_frame = self.remove_false_center(frame, center)
 
-                        new_frame[position, position2] = 0
+        return new_frame
+
+    def remove_false_center(self, frame, center):
+        new_frame = np.copy(frame)
+        i, j = center
+        new_frame[i, j] = self.new_pixel
+
+        validate = True
+        increment = 0
+        while validate:
+            increment += 1
+            new_frame[i+increment, j] = new_frame[i-increment, j] = self.new_pixel
+            new_frame[i, j+increment] = new_frame[i, j-increment] = self.new_pixel
+
+            validate = (new_frame[i+2, j] == self.pixel_remove or new_frame[i-2, j] == self.pixel_remove) or (
+                    new_frame[i, j+2] == self.pixel_remove or new_frame[i, j-2] == self.pixel_remove)
 
         return new_frame
