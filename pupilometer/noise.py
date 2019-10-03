33c2e9ef2f599fc9ee14f6cc4e3c181b67264f49
Marcos Vinicius <marcos.v.silva@live.com>
2019-10-02 21:19:21 -0300

    Alter points for verify contours

--- pupilometer/noise.py
+++ pupilometer/noise.py
@@ -18,7 +18,7 @@ class Noise:
 
         center, _ = self.ellipse.search_ellipse(frame, contours)
 
-        if frame[center[0], center[1]] == self.pixel_remove:
+        if frame[center[1], center[0]] == self.pixel_remove:
             new_frame = self.remove_false_center(frame, center)
 
         return new_frame
@@ -26,14 +26,19 @@ class Noise:
     def remove_false_center(self, frame, center):
         new_frame = np.copy(frame)
         i, j = center
+        lin, col = frame.shape
         new_frame[i, j] = self.new_pixel
 
         validate = True
         increment = 0
         while validate:
             increment += 1
-            new_frame[i+increment, j] = new_frame[i-increment, j] = self.new_pixel
-            new_frame[i, j+increment] = new_frame[i, j-increment] = self.new_pixel
+
+            if i + increment >= lin or j + increment >= col:
+                break
+
+            new_frame[j, i+increment] = new_frame[j, i-increment] = self.new_pixel
+            new_frame[j+increment, i] = new_frame[j-increment, i] = self.new_pixel
 
             validate = (new_frame[i+2, j] == self.pixel_remove or new_frame[i-2, j] == self.pixel_remove) or (
                     new_frame[i, j+2] == self.pixel_remove or new_frame[i, j-2] == self.pixel_remove)
